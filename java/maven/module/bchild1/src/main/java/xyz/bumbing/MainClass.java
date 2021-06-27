eb389c6e313951b43b451b5d0a2ae988649fbb6f
Han <kjhhjk1234@naver.com>
2021-06-27 21:11:21 +0900

    메이븐 멀티 모듈 공부

---
+++ java/maven/module/bchild1/src/main/java/xyz/bumbing/MainClass.java
@@ -0,0 +1,9 @@
+package xyz.bumbing;
+
+public class MainClass {
+  public static void main(String[] args) {
+
+
+
+  }
+}
---
+++ java/maven/module/bchild2/src/main/java/xyz/bumbing/bch2/Main.java
@@ -0,0 +1,9 @@
+package xyz.bumbing.bch2;
+
+public class Main {
+
+  public static void main(String[] args) {
+    iControl.XTrustProvider.install();
+    iControl.Interfaces aa = new iControl.Interfaces();
+  }
+}
